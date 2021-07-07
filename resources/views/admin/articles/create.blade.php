a87cf1b90bd05a8fb32cc33f13c17a0d364a81a8
Md Rezaul Karim <rkreza24@gmail.com>
2021-07-07 16:08:35 +0600

    Auto Slug Generate From Title & Category Link Added

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -209,4 +209,13 @@
 	    });
 
     </script>
+
+    <script type="text/javascript">
+
+		$("#title").keyup(function(){
+    		var title = this.value;
+    		title = title.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
+			$("#slug").val(title);
+		})
+	</script>
 @endpush

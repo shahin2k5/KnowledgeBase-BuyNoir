33bdbc1ae265d5d6702d0f38d66534b70919cfd3
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-07-13 12:02:28 +0600

    favicon fixing for home page

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -17,7 +17,12 @@
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
         
-
+	 <!-- Favicon -->
+		@if($setting->website_favicon != null || !empty($setting->website_favicon))
+			<link rel="shortcut icon" type="image/x-icon" href="{{$setting->website_favicon}}">
+		@else
+			<link rel="shortcut icon" type="image/x-icon" href="/assets/admin/img/favicon.png">
+		@endif
         <!-- Stylesheets -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ asset('assets/frontend/css/style.css') }}">

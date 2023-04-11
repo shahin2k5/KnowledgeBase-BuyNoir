edba7d6947e443c8f8473561197cbde2bc18bb61
ShourovRootNext <126763048+ShourovRootNext@users.noreply.github.com>
2023-04-11 13:09:55 +0600

    Issue:BUY-69 & BUY-79 in Jira is solved

--- resources/views/frontend/page.blade.php
+++ resources/views/frontend/page.blade.php
@@ -55,18 +55,18 @@
 				<div class="row">
                     <div class="col">
                         <div class="px-4 py-5 px-lg-5 bg-white">
-                            <h3 class="mb-3 text-primary-dark"><b>{{$page->title}}</b></h3>
+                            <h3 class="mb-3" style="font-weight: bolder; color: #0ea1e2"><b>{{$page->title}}</b></h3>
                             <div class="row align-items-center gx-2 mb-4">
                                 <div class="col-auto">
-                                    <div class="bg-primary text-white fw-bold text-center rounded-circle"
-                                        style="width: 35px; height: 35px; line-height: 35px">P</div>
+                                    <div class="text-black fw-bold text-center rounded-circle"
+                                        style="width: 35px; height: 35px; line-height: 35px; background-color: #ebf7fc;">P</div>
                                 </div>
                                 <div class="col">
 									@php
 										$user = \App\Models\User::find($page->user_id);
 									@endphp
-                                    <p class="m-0 small"><b>Written by   {{$user->name}} </b></p>
-                                    <p class="m-0 small">Last updated: {{date('Y-m-d', strtotime($page->updated_at));}}</p>
+                                    <p class="m-0 small" style="font-size: 16px; color: #0ea1e2"><b>Written by   {{$user->name}} </b></p>
+                                    <p class="m-0 small" style="font-size: 16px;">Last updated: {{date('Y-m-d', strtotime($page->updated_at));}}</p>
                                 </div>
                             </div>
 
